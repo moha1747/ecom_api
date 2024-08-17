e941d412e54aabc0037a019e3cfc026e19414629
moha1747 <mohamud.abbaas14@gmail.com>
2024-08-17 03:02:46 -0500

    New ENV variables

--- config/env.go
+++ config/env.go
@@ -3,6 +3,8 @@ package config
 import (
 	"fmt"
 	"os"
+	"strconv"
+
 	"github.com/lpernett/godotenv"
 )
 
@@ -13,6 +15,8 @@ type Config struct {
 	DBPassword string
 	DBAddress string
 	DBName string
+	JWTExpirationInSeconds int64
+	JWTSecret string
 }
 
 var Envs = initConfig()
@@ -27,12 +31,24 @@ godotenv.Load()
 		DBAddress: fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), 
 		getEnv("DB_PORT", "3306")),
 		DBName: getEnv("DB_NAME", "ecom"),
+		JWTExpirationInSeconds: getEnvAsInt("JWT_EXP", 3600 * 24 * 7),
 	}
 }
 
 func getEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
+	}
+	return fallback
+}
+
+func getEnvAsInt (key string, fallback int64) int64 {
+	if value, ok := os.LookupEnv(key); ok {
+		i, err := strconv.ParseInt(value, 10, 64)
+		if err != nil {
+			return fallback
+		}
+		return i
 	}
 	return fallback
 }
